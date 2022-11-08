6fcb8726114eba53e3562e48accc1954a96635fb
Max dc <mcangreq@outlook.com>
2022-11-07 21:23:18 -0500

    five

---
+++ java/main.js
@@ -0,0 +1,17 @@
+let open = document.querySelector('.fa-solid');
+let flotante = document.querySelector('.nav_phone');
+let close = document.querySelector('.closep');
+
+let color = document.querySelector('.tl')
+ 
+
+open.addEventListener('click',(e)=>{
+    flotante.classList.toggle('active');
+  open.classList.toggle('opacity')
+})
+
+
+
+
+
+
