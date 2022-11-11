ed86d30ab63da9fe50cd96af6473565407fc99fb
Max dc <mcangreq@outlook.com>
2022-11-10 20:57:46 -0500

    create pag inicio

--- java/main.js
+++ java/main.js
@@ -3,14 +3,21 @@ let open = document.querySelector('.fa-solid');
 let flotante = document.querySelector('.nav_phone');
 let close = document.querySelector('.closep');
 let closep = document.querySelector('body')
+let closeX = document.querySelector('.closep_x')
 
 let color = document.querySelector('.tl')
  
 
-open.addEventListener('click',(e)=>{
-    flotante.classList.toggle('active');
+// open.addEventListener('click',(e)=>{
+//     flotante.classList.toggle('active');
   
-})
+// })
+ open.addEventListener('click',()=>{
+    flotante.style.transform='translate(0rem)'
+ })
+ closeX.addEventListener('click',()=>{
+    flotante.style.transform='translate(-30rem)'
+ })
 
 /// formulario
 let formulario = document.querySelector('.formulario')
@@ -28,11 +35,3 @@ closeIcon.addEventListener('click',()=>{
     formulario.style.transform='scale(0)'
 })
 
-$(document).ready(function () {
-    function reorient(e) {
-      var portrait = (window.orientation % 180 == 0);
-      $("body > div").css("-webkit-transform", !portrait ? "rotate(-90deg)" : "");
-    }
-    window.onorientationchange = reorient;
-    window.setTimeout(reorient, 0);
-  });
